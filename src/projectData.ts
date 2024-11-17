a18f3f1f2f3e092cf722b1404def88471991f742
Jamie McCallum <jamiemccallum0@gmail.com>
2024-11-17 00:25:03 +0000

    moved projects folder

--- src/projectData.ts
+++ src/projectData.ts
@@ -1,14 +1,16 @@
+const BASE_PATH = '/scrimba-dashboard';
+
 export const projectsData = {
     counterApp: {
         title: 'Counter App',
         description: 'This app allows you to increment and save a counter',
-        liveDemoLink: 'projects/counter-app/index.html',
+        liveDemoLink: `${BASE_PATH}/projects/counter-app/index.html`,
         technologies: ['HTML', 'CSS', 'JavaScript']
     },
     scoreboard: {
         title: 'Basketball Scoreboard',
         description: 'Digital scoreboard with timer and period tracking',
-        liveDemoLink: 'projects/solo-scoreboard/index.html',
+        liveDemoLink: `${BASE_PATH}/projects/solo-scoreboard/index.html`,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         features: [
             'Score tracking',
