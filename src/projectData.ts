e5a0e621adff7b7920476f78339e8549732a1cbd
Jamie McCallum <jamiemccallum0@gmail.com>
2024-11-17 00:33:42 +0000

    updating projects

--- src/projectData.ts
+++ src/projectData.ts
@@ -1,14 +1,27 @@
 const BASE_PATH = '/scrimba-dashboard';
 
 export const projectsData = {
+    businessCard: {
+        title: '1. Business Card',
+        description: 'This is a business card with some animations',
+        liveDemoLink: `${BASE_PATH}/projects/business-card/index.html`,
+        technologies: ['HTML', 'CSS']
+    },
+    basicWebsite: {
+        title: '2. Personal Site',
+        description: 'A personal website about me',
+        liveDemoLink: `${BASE_PATH}/projects/personal-site/index.html`,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    
     counterApp: {
-        title: 'Counter App',
+        title: '3. Counter App',
         description: 'This app allows you to increment and save a counter',
         liveDemoLink: `${BASE_PATH}/projects/counter-app/index.html`,
         technologies: ['HTML', 'CSS', 'JavaScript']
     },
     scoreboard: {
-        title: 'Basketball Scoreboard',
+        title: '4. Basketball Scoreboard (solo project)',
         description: 'Digital scoreboard with timer and period tracking',
         liveDemoLink: `${BASE_PATH}/projects/solo-scoreboard/index.html`,
         technologies: ['HTML', 'CSS', 'JavaScript'],
